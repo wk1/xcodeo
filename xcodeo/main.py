512393189aba5cff8edf86da8fd3242e893ad7d8
Christian Hoock <9932295+wk1@users.noreply.github.com>
2024-05-06 16:02:12 +0200

    Add path argument

--- xcodeo/main.py
+++ xcodeo/main.py
@@ -5,8 +5,8 @@ import sys
 import inquirer
 import argparse
 
-def find_files(extension):
-    return [f for f in os.listdir('.') if f.endswith(extension)]
+def find_files(extension, path):
+    return [f for f in os.listdir(path) if f.endswith(extension)]
 
 def open_with_xcode(file_path):
     subprocess.run(["open", "-a", "Xcode", file_path])
@@ -24,12 +24,14 @@ def interactive_select(files):
 def main():
     parser = argparse.ArgumentParser(description="Tool zur Interaktion mit Xcode-Projekten. Dieses Tool ermöglicht es, .xcodeproj oder .xcworkspace Dateien auszuwählen und in Xcode zu öffnen.",
                                      epilog="Beispielgebrauch: xcodeo --project oder xcodeo -w für workspaces.")
-
-    parser.add_argument('-p', '--project', action='store_true', 
+    
+    parser.add_argument('path', type=str, 
+                        help="Pfad zu den .xcodeproj/.xcworkspace Dateien.")
+    parser.add_argument('-p', '--project', action='store_true',
                         help="Verarbeite .xcodeproj Dateien (standardmäßig).")
     parser.add_argument('-w', '--workspace', action='store_true', 
                         help="Verarbeite .xcworkspace Dateien.")
-
+    
     args = parser.parse_args()
 
     # Default to .xcodeproj unless --workspace is specified
@@ -39,7 +41,7 @@ def main():
         extension = ".xcodeproj"  # Default behavior
 
     print(f"Verarbeite Dateien mit der Endung: {extension}")
-    files = find_files(extension)
+    files = find_files(extension, args.path)
 
     if len(files) == 0:
         print(f"Keine {extension} Dateien gefunden.")
